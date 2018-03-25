df6cb89546b446f1c65faafbcb8fde89e6e0d88b
Aly Machaca <alyaj2a@gmail.com>
2018-03-24 22:53:05 -0500

    Added Menu Sticky

--- layout/scripts/menu_fixed.js
+++ layout/scripts/menu_fixed.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 	var altura = $('.menu_main').offset().top;
+
 	$(window).on('scroll', function(){
-		if ( $(window).scrollTop() > altura){
+		if ( $(window).scrollTop() > altura ){
 			$('.menu_main').addClass('menu-fixed');
 		} else {
 			$('.menu_main').removeClass('menu-fixed');
 		}
 	});
+
 });
